6f7922d3efbb5141e82e8f366b379b119c94c8d2
AliSajid321 <alisajidsiddique@gmail.com>
2022-01-05 12:45:10 +0500

    Added functionality

--- game.js
+++ game.js
@@ -1,6 +1,66 @@
 var buttonColors= ["red","blue","green","yellow"];
 var gamePattern = [];
+var userClickedPattern = [];
 
+$(".btn").click(function(){
+
+        
+    var userChosenColor = $(this).attr("id");
+    userClickedPattern.push(userChosenColor);
+    //console.log(userClickedPattern);
+    playSound(userChosenColor);
+    animatePress(userChosenColor);
+    checkAnswer(userClickedPattern.length - 1);
+
+});
+
+var count = 0;
+var level = 0;
+
+$(document).keypress(function(){
+    
+    if (count===0){
+        nextSequence();
+        $("h1").text("Level 0");
+        count++;
+        level--;
+    }
+    else {
+        count = 1;
+    }
+    
+});
+
+function playSound(name){
+    var colorSound = new Audio("sounds/"+ name + ".mp3");
+    colorSound.play();
+}
+
+function animatePress(currentColor){
+    var checkPress = $("#"+currentColor).addClass("pressed");
+    setTimeout(function(){
+        checkPress.removeClass("pressed");
+    }, 100);
+
+}
+
+function checkAnswer(currentLevel){
+    if(userClickedPattern[currentLevel]===gamePattern[currentLevel]){
+        console.log("success");
+        if(userClickedPattern.length===gamePattern.length){
+            setTimeout(function(){
+                nextSequence();
+            },1000);
+            userClickedPattern=[];
+
+        }
+        
+    }
+    else{
+        console.log("fail");
+    }
+
+}
 
 
 function nextSequence(){
@@ -8,6 +68,14 @@ function nextSequence(){
     randomNumber = Math.floor(randomNumber);
     var randomChosenColor = buttonColors[randomNumber];
     gamePattern.push(randomChosenColor);
-    console.log(gamePattern);
-    
+    //console.log(gamePattern);
+    $("#"+ randomChosenColor).fadeOut(30).fadeIn(30);
+    playSound(randomChosenColor);
+    level++;
+    $("h1").text("Level "+ level);
+    // var colorSound = new Audio("sounds/"+ randomChosenColor + ".mp3");
+    // colorSound.play();
+    //return randomChosenColor;
+
+
 }
